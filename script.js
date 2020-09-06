b874536e5b0d591d804715f8e7c271336a8c9841
MarcGlug <marc.boyon@hotmail.fr>
2020-09-06 23:13:20 +0200

    Mise en place bouton organize

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ window.onload = function(){
 var randBtn = document.getElementById("randomize");
 randBtn.addEventListener("click", () => {randomize()} );
 
+var orgBtn = document.getElementById("organize");
+orgBtn.addEventListener("click", () => {organize()} );
+
 
 function randomize() {
     for (let i = 0; i < 101; i++) {
@@ -13,5 +16,8 @@ function randomize() {
     }
 }
 
+function organize() {
+    alert("Sorting isn't ready yet");
+}
 
 }
